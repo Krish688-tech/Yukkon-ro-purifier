a1cc47e0ad123252aaa6de86376e2389d4143de4
unknown <littlelotus04@gmail.com>
2026-04-20 19:34:36 +0530

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,16 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 import { HelmetProvider } from "react-helmet-async";
-import { WishlistProvider } from "./context/WishlistContext";
+
 
 createRoot(document.getElementById("root")).render(
   <HelmetProvider>
     <BrowserRouter>
       <AuthProvider>
         <CartProvider>
-          <WishlistProvider>
             <App />
-          </WishlistProvider>
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
